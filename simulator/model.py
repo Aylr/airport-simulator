dbbee9057ede5b06901aa995555e5f177a524d92
Aylr <Aylr@users.noreply.github.com>
2019-05-16 00:25:14 -0600

    * planes dock at stands and change their state

--- simulator/model.py
+++ simulator/model.py
@@ -13,9 +13,9 @@ class Airline(Agent):
         super().__init__(unique_id, model)
         self.airline_type = random.choice([1, 2])
         self.in_line = True
-        self.at_stand = False
         self.time_at_stand = random.randint(20, 30)
         self.verbose = verbose
+        self.tick_count = 0
 
     @property
     def x_position(self):
@@ -26,16 +26,21 @@ class Airline(Agent):
         return self.pos[1]
 
     def step(self):
+        self.tick_count += 1
+
         if self.verbose:
-            print(f"plane {self.unique_id}: {self.at_stand} for {self.time_at_stand}")
-        if self.at_stand:
-            if self.time_at_stand > 0:
-                self.time_at_stand -= 1
-            else:
+            print(f"plane {self.unique_id}: {self.is_at_stand} for {self.time_at_stand}")
+        if self.is_at_stand:
+            # TODO this isn't super efficient to do this every step
+            self.model.add_plane_to_stand(self)
+
+            if self.time_at_stand <= 0:
                 # Remove the plane from the simulation
                 if self.verbose:
                     print(f"Plane {self.unique_id} leaving stand")
-                self.model.schedule.remove(self)
+                self.model.remove_plane(self)
+
+            self.time_at_stand -= 1
         else:
             self.move()
 
@@ -67,22 +72,28 @@ class Airline(Agent):
         min_indices = [i for i, x in enumerate(deltas) if x == min(deltas)]
         return [stands[i] for i in min_indices]
 
+    @property
+    def is_at_stand(self):
+        for id, stand in self.model.stands.items():
+            if self.pos == stand.position:
+                return True
+
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
             self.pos, moore=False, include_center=False
         )
-        # TODO move towards open stand
-        # TODO move towards closest open stand
         open_stands = self.model.get_open_stands(self.airline_type)
         closest_stands = self.closest_stands(open_stands)
 
-        picked_stand = random.choice(closest_stands)
-        steps_closer = self.get_moves_closer(possible_steps, picked_stand)
-        if steps_closer:
-            new_position = self.random.choice(steps_closer)
-            if self.verbose:
-                print(f"moving plane from {self.pos} to {new_position}")
-            self.model.grid.move_agent(self, new_position)
+        if closest_stands:
+            picked_stand = random.choice(closest_stands)
+            steps_closer = self.get_moves_closer(possible_steps, picked_stand)
+
+            if steps_closer:
+                new_position = self.random.choice(steps_closer)
+                if self.verbose:
+                    print(f"moving plane from {self.pos} to {new_position}")
+                self.model.grid.move_agent(self, new_position)
 
 
 class Stand(object):
@@ -93,15 +104,23 @@ class Stand(object):
         self.airline_type = airline_type
         self.x = x
         self.y = y
-        self.is_occupied = None
+        self.is_occupied = False
+
+    @property
+    def position(self):
+        return (self.x, self.y)
 
     def __repr__(self):
-        return f"<Stand {self.unique_id} accepts type: {self.airline_type} slot: {self.is_occupied}>"
+        return f"<Stand {self.unique_id} accepts type: {self.airline_type} is_occupied: {self.is_occupied}>"
 
 
 class AirportModel(Model):
     def __init__(self, n, width=20, height=20, verbose=True):
+        # TODO probability of adding plane on a tick
         self.number_planes = n
+        self.width = width
+        self.max_plane_id = 0
+        self.verbose = verbose
         self.schedule = RandomActivation(self)
         # TODO raise error on smallest grid
         self.grid = MultiGrid(width, height, torus=False)
@@ -116,27 +135,50 @@ class AirportModel(Model):
             7: Stand(7, 2, width - 1, height - 10),
         }
 
-        # create planes
-        for i in range(self.number_planes):
-            plane = Airline(i, self, verbose=verbose)
-            self.schedule.add(plane)
-            middle_x = int(width / 2)
-            self.grid.place_agent(plane, (middle_x, 0))
-
         self.datacollector = DataCollector(
-            model_reporters={"number_planes": "number_planes"},
-            agent_reporters={"x": "x_position", "y": "y_position"},
+            # TODO stand metrics
+            # TODO planes served
+            # TODO planes queued
+            # TODO queue duration
+            model_reporters={"number_planes": "number_planes", },
+            agent_reporters={
+                "tick_count": "tick_count",
+                "x": "x_position",
+                "y": "y_position",
+                "is_at_stand": "is_at_stand",
+                "time_at_stand": "time_at_stand",
+            },
         )
 
+    def add_plane(self):
+        plane = Airline(self.max_plane_id, self, verbose=self.verbose)
+        self.max_plane_id += 1
+
+        self.schedule.add(plane)
+        middle_x = int(self.width / 2)
+        self.grid.place_agent(plane, (middle_x, 0))
+
     def step(self):
+        if random.random() >= 0.7:
+            self.add_plane()
+
         self.datacollector.collect(self)
         self.schedule.step()
 
-    def remove_plane_from_stand(self, plane):
-        pass
+    def remove_plane(self, plane):
+        for id, stand in self.stands.items():
+            if plane.pos == stand.position:
+                self.stands[id].is_occupied = False
+                break
+        self.schedule.remove(plane)
 
-    def add_plane_to_stand(self):
-        pass
+    def add_plane_to_stand(self, plane):
+        for id, stand in self.stands.items():
+            if plane.pos == stand.position:
+                self.stands[id].is_occupied = True
+                print(f"Plane {plane.unique_id} docked at stand {id}")
+                print(self.stands)
+                break
 
     def get_stands_of_type(self, airline_type):
         return [s for id, s in self.stands.items() if s.airline_type == airline_type]
@@ -175,9 +217,9 @@ class AirportModel(Model):
 
 
 if __name__ == "__main__":
-    airport = AirportModel(10, width=50, height=40, verbose=False)
+    airport = AirportModel(6, width=20, height=20, verbose=False)
     print(airport)
-    for _ in range(100):
+    for _ in range(50):
         airport.step()
     print(f"{len(airport.schedule.agents)} planes left in stands")
     print(airport.stands)
@@ -185,3 +227,4 @@ if __name__ == "__main__":
     # airport.plot_positions()
     airport.plot_position_history()
     # plt.show()
+    df = airport.datacollector.get_agent_vars_dataframe()
